84ad1bd14e6700e31881422e8e6047f150fd9ff4
lovedeep5 <lovedeep5.abh@gmail.com>
2022-08-31 17:28:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const requestIp = require("request-ip");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+app.get("/", (req, res) => {
+  const ip = requestIp.getClientIp(req);
+  res.json({ ip });
+});
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
